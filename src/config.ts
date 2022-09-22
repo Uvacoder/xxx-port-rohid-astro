c2e20d7931ece680a3719e0be38822adf881fcbb
Rohidul Islam <rohidul209@gmail.com>
2022-09-22 23:34:25 +0600

    old posts added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Rohid";
+export const SITE_DESCRIPTION = "I'm a self-thought full-stack developer";
